6625edb694a77cad2ce059a93d07c9fc574043df
Nijoane <nicajoane@outlook.com>
2021-03-18 19:56:27 -0300

    changing port

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
-const express = require('express')
-const routes = require('./server/routes/index')
-const app = express()
-const port = 3000
+const express = require('express');
+const routes = require('./server/routes/index');
+
+const app = express();
+const port = 3333;
 
 app.use('/', routes);
 
-app.get('*', (req, res) => {
-  res.send('Hello World!')
+app.get('/', (request, response) => { 
+  response.json({message: 'Hello World!'});
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+  `${port}`;
 })
