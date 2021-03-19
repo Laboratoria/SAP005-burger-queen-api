fe214aed3657ccd79cccea8031ddfb1c2910eab1
akemimeka <akemimeka@gmail.com>
2021-03-19 17:03:18 -0300

    Instala e configura ESLint no projeto

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
-const express = require('express')
-const routes = require('./server/routes/index')
-const app = express()
-const port = 3000
+const express = require('express');
+const routes = require('./server/routes/index');
+
+const app = express();
+const port = 3000;
 
 app.use('/', routes);
 
 app.get('*', (req, res) => {
-  res.send('Hello World!')
-})
+  res.send('Hello World!');
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+  console.log(`Example app listening at http://localhost:${port}`);
+});
