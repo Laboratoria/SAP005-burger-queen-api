88da808ea06a7ad3e271c7ffdfa9583a95fc0710
Nijoane <nicajoane@outlook.com>
2021-03-19 16:50:52 -0300

    adding project url to port

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const routes = require('./server/routes/index');
 
 const app = express();
-const port = 3333;
+const port = 'https://tag-burger-api.herokuapp.com/';
 
 app.use('/', routes);
 
