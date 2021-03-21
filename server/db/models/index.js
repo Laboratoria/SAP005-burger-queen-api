78d97cff23fe52598ac394a0858831cb03995ad9
Lu-Pereira <luci.lv14@gmail.com>
2021-03-21 01:56:40 -0300

    adicionado cofig docker e heroku

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -17,6 +17,16 @@ if (config.use_env_variable) {
   sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
 
+// teste de conexão;
+// async function teste() {
+//   try {
+//     await sequelize.authenticate();
+//     console.log('Connection has been established successfully.');
+//   } catch (error) {
+//     console.error('Unable to connect to the database:', error);
+//   }
+// } teste();
+
 fs
   .readdirSync(__dirname)
   .filter((file) => (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js'))
