c4bc58c1d76ba3c308fa101ded1c0ee0e5a4bc6b
akemimeka <akemimeka@gmail.com>
2021-03-21 22:55:06 -0300

    Cria arquivo orders.js em routes

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,30 +1,12 @@
 const express = require('express');
-const { Router } = require('express');
 
 const app = express();
 const users = require('./users');
 const products = require('./products');
-
-const router = Router();
+const orders = require('./orders');
 
 app.use('/users', users);
 app.use('/products', products);
-
-router
-  .get('/orders', (req, res) => {
-    res.send('Pega dados de todos os pedidos.');
-  })
-  .post('/orders', (req, res) => {
-    res.send('Cria um novo pedido.');
-  })
-  .get('/orders/:orderId', (req, res) => {
-    res.send(`Pega dados do pedido ${req.params.orderId}.`);
-  })
-  .put('/orders/:orderId', (req, res) => {
-    res.send(`Atualiza pedido ${req.params.orderId}.`);
-  })
-  .delete('/orders/:orderId', (req, res) => {
-    res.send(`Deleta pedido ${req.params.orderId}.`);
-  });
+app.use('/orders', orders);
 
 module.exports = app;
