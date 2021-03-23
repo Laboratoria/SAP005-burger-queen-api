f01be514b6e50c21f6d69666ff9a9ed92920bdc4
akemimeka <akemimeka@gmail.com>
2021-03-23 11:31:02 -0300

    Cria model para Orders e conecta com user_id do model Users

---
+++ server/db/models/orders.js
@@ -0,0 +1,25 @@
+/* eslint-disable linebreak-style */
+
+const { Model } = require('sequelize');
+
+module.exports = (sequelize, DataTypes) => {
+  class Orders extends Model {
+    static associate(models) {
+      Orders.belongsTo(models.Users, {
+        foreignKey: 'user_id',
+      });
+    }
+  }
+
+  Orders.init({
+    user_id: DataTypes.INTEGER,
+    client_name: DataTypes.STRING,
+    table: DataTypes.INTEGER,
+    status: DataTypes.STRING,
+    processedAt: DataTypes.DATE,
+  }, {
+    sequelize,
+    modelName: 'Orders',
+  });
+  return Orders;
+};
--- server/db/models/users.js
+++ server/db/models/users.js
@@ -3,16 +3,8 @@
 const { Model } = require('sequelize');
 
 module.exports = (sequelize, DataTypes) => {
-  class Users extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
-    }
-  }
+  class Users extends Model {}
+
   Users.init({
     name: DataTypes.STRING,
     email: DataTypes.STRING,
