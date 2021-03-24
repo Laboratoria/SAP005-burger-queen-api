2c8b47dea21179124433c5c1b6bd986e2e328e35
edilene <edilenefern@gmail.com>
2021-03-24 11:18:39 -0300

    exibindo dados

--- server/controller/ExampleController.js
+++ server/controller/ExampleController.js
@@ -1,8 +1,8 @@
 // aqui vai o código que acessa o banco de dados
 
 const getAllExamples = (req, res) => {
-  console.log("você também pode utilizar o console para visualizar =)")
-  res.send("Request feita ")
+  return res.json(["Maria","Marta","Joana"]);
 }
+
 
 module.exports = { getAllExamples }
