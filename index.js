83ac5081d6658a32429f71f41f54648b1a3fab04
KarinaFS <karina_fsantos@outlook.com>
2021-03-28 17:18:21 -0300

    port

--- index.js
+++ index.js
@@ -3,25 +3,7 @@ const routes = require('./server/routes/index')
 const app = express()
 const port = 3000
 
-app.use('/', routes);
+app.use(express.json());
+app.use(routes);
 
-const users = [
-  { id: 1, 
-    username: 'Pedro Pinto', 
-    email: 'ppinto@ppl.com' },
-  { id: 2, 
-    username: 'Francisco', 
-    email: 'francisco@ppl.com' }, 
-  { id: 3, 
-    username: 'Carla Figueiredo', 
-    email: 'cfig@ppl.com' }
-];
-
-app.get('/', (req, res) => {
-  res.send(users)
-})
-
-
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+app.listen(port);
