4bb12fbb76163a5bc5cb373da9abdfff1cf2e190
KarinaFS <karina_fsantos@outlook.com>
2021-03-28 13:50:45 -0300

    create user

--- server/controller/UsersController.js
+++ server/controller/UsersController.js
@@ -1,22 +1,48 @@
 const database = require("../db/models")
+const models = require("../db/models")
+
 
 class UsersController {
-  static async getAllUsers (req, res) {
-  const users = await database.User.findAll()
-    return res.status(200).json(users)
+  static async getAllUsers(req, res) {
+    try {
+      const users = await database.User.findAll()
+      return res.status(200).json(users)
+    } catch (err) {
+      return res.status(400).json({ error: "dados não encontrados" })
+    }
   }
 
   static async getUserById(req, res) {
-    const { uid } = req.params 
-    const user = await database.User.findAll({
-      where: {
-        id: Number(uid)
-      }
-    });
-    return res.status(200).json(user)
+    const { uid } = req.params
+    try {
+      const userId = await database.User.findAll({
+        where: {
+          id: Number(uid)
+        }
+      });
+      return res.status(200).json(userId)
+    } catch (error) {
+      return res.status(400).json({ error: "dados não encontrados" })
+    }
   }
 
-  
+  static async createUser(req, res) {
+    try {
+      const { userName, email, password, role, restaurant } = req.body;
+      const createUser = await models.User.create({
+        userName,
+        email,
+        password,
+        role,
+        restaurant
+      })
+      return res.status(200).json(createUser);
+    } catch (error) {
+      return res.status(400).json({ error: "Não foi possível criar um usuário" })
+    }
+  }
+
+
 }
 
 module.exports = UsersController
--- server/routes/UsersRouter.js
+++ server/routes/UsersRouter.js
@@ -4,7 +4,7 @@ const router = Router()
 
 router.get('/', UsersController.getAllUsers);
 router.get('/:uid', UsersController.getUserById);
-// router.post('/', UsersController.createUser);
+router.post('/', UsersController.createUser);
 // router.put('/:uid', UsersController.updateUser);
 // router.delete('/:uid', UsersController.deleteUser);
 
