83f113257f61c82ab86be7698a5392f4e5d54acd
Carol Shimada <caroline_shimada@hotmail.com>
2021-03-27 23:04:39 -0300

    parte de usuario concluido faltando apenas tratar bug de email repetido

--- server/controller/userController.js
+++ server/controller/userController.js
@@ -6,53 +6,29 @@ const { resolve } = require("path");
 const { nextTick } = require("process");
 const models = require('../db/models')
 
-const bancoDadosUser = [
-  {
-    "id":1,
-    "name": "Carol",
-    "sobrenome": "Shimada",
-  },
-  {
-    "id":2,
-    "name": "Rayan",
-    "sobrenome": "Salewski",
-  },
-  {
-    "id":3,
-    "name": "Joana",
-    "sobrenome": "Santos",
-  }
-]
-
-//Pegar todos os usuários ---------------------- ok
 const getAllUsers = async (req, res, next) => {
   try {
     const users = await models.User.findAll();
-    //res.status(200).send(users)
     res.status(200).json(users);
   } catch (err){
     next(err)
   }
 }
 
-
-//Pegar usuário por ID
 const getUserId = async (req, res, next) => {
-  try{
+  try {
     const users = await models.User.findAll({
       where: 
       {id:req.params.id}
     })
     res.status(200).json(users);
-    //res.status(200).send(users)
     } catch (err){
       next(err);
     }
 }
-
-//Criar Usuário ---------------------- ok
+//tratar e-mails repetidos
 const postUser = async (req, res, next) => {
-  try{
+  try {
     const { name, email, password, role, restaurant } = req.body;
     const user = await models.User.create({
       name,
@@ -65,26 +41,43 @@ const postUser = async (req, res, next) => {
     })
     res.status(200).json(user);
   } catch(err){
-    next(err)
-  }
-}
-//Alterar usuário
-const updateUser = (req, res) => {
-  console.log("você também pode utilizar o console para visualizar =)")
-  res.send("Request Update UserId")
-}
-
-//Deletar usuário ---------------------- atual
-const deleteUsers = (req, id) => {
-  var result = bancoDadosUser.filter(function(el) {
-    return el.id == id;
-  });
-    
-  for(var elemento of result){
-    var index = bancoDadosUser.indexOf(elemento);    
-    bancoDadosUser.splice(index, 1);
+    next(err);
   }
 }
 
+const updateUser = async (req, res, next) => {
+  try {
+    const {id, name, email, password, role, restaurant } = req.body;
+    const users = await models.User.update(
+      { 
+        id,
+        name, 
+        email, 
+        password, 
+        role, 
+        restaurant 
+      },
+      {
+        where:
+        { id: req.params.id }
+      } 
+    )
+    return res.json(users);
+  } catch(err) {
+    next(err);
+  }
+}
+
+const deleteUsers = async (req, res, next) => {
+  try {
+    const users = await models.User.destroy({
+      where: 
+      {id:req.params.id}
+    });
+    return res.json(users);
+  } catch(err) {
+    next(err);
+  }
+}
 
 module.exports = { getAllUsers, postUser, deleteUsers, getUserId, updateUser}
