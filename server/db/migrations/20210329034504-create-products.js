56d0073c936bb0d76727574091675bf94bf06eea
KarinaFS <karina_fsantos@outlook.com>
2021-03-29 00:47:22 -0300

    criação da model products

---
+++ server/db/migrations/20210329034504-create-products.js
@@ -0,0 +1,45 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Products', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      price: {
+        type: Sequelize.INTEGER
+      },
+      flavor: {
+        type: Sequelize.STRING
+      },
+      complement: {
+        type: Sequelize.STRING
+      },
+      image: {
+        type: Sequelize.STRING
+      },
+      type: {
+        type: Sequelize.STRING
+      },
+      sub_type: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Products');
+  }
+};
---
+++ server/db/models/products.js
@@ -0,0 +1,28 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Products extends Model {
+    static associate(models) {
+      Products.belongsToMany(models.Orders, {
+        through: 'ProductsOrders',
+        as: 'orders',
+        foreignKey: 'productId',
+      });
+    }
+  };
+  Products.init({
+    name: DataTypes.STRING,
+    price: DataTypes.INTEGER,
+    flavor: DataTypes.STRING,
+    complement: DataTypes.STRING,
+    image: DataTypes.STRING,
+    type: DataTypes.STRING,
+    sub_type: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'Products',
+  });
+  return Products;
+};
