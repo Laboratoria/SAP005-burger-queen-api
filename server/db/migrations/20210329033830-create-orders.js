80d45bded0dae621432f693d294966cc9e76caef
KarinaFS <karina_fsantos@outlook.com>
2021-03-29 00:42:24 -0300

    criação da model orders

---
+++ server/db/migrations/20210329033830-create-orders.js
@@ -0,0 +1,33 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('Orders', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      client_name: {
+        type: Sequelize.STRING
+      },
+      table: {
+        type: Sequelize.INTEGER
+      },
+      status: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('Orders');
+  }
+};
---
+++ server/db/models/orders.js
@@ -0,0 +1,25 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Orders extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  };
+  Orders.init({
+    client_name: DataTypes.STRING,
+    table: DataTypes.INTEGER,
+    status: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'Orders',
+  });
+  return Orders;
+};
