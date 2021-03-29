36b12f52ccdd18f5b0f31d478cddc24e15ab3879
KarinaFS <karina_fsantos@outlook.com>
2021-03-29 00:52:16 -0300

    criação da model products orders

---
+++ server/db/migrations/20210329035024-create-products-orders.js
@@ -0,0 +1,30 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('ProductsOrders', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      productId: {
+        type: Sequelize.INTEGER
+      },
+      qtd: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('ProductsOrders');
+  }
+};
---
+++ server/db/models/productsorders.js
@@ -0,0 +1,18 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class ProductsOrders extends Model {
+    static associate(models) {
+    }
+  };
+  ProductsOrders.init({
+    productId: DataTypes.INTEGER,
+    qtd: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'ProductsOrders',
+  });
+  return ProductsOrders;
+};
