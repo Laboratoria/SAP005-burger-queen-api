7fb02e745a84a6597697324175d904745d8fe09a
KarinaFS <karina_fsantos@outlook.com>
2021-03-30 12:08:54 -0300

    buscando produto pelo id

--- server/controller/ProductsController.js
+++ server/controller/ProductsController.js
@@ -1,5 +1,5 @@
 const database = require("../db/models")
-// const models = require("../db/models")
+const models = require("../db/models")
 
 class ProductsController {
   static async getAllProducts(req, res) {
@@ -7,7 +7,21 @@ class ProductsController {
       const getProducts = await database.Products.findAll()
       return res.status(200).json(getProducts);
     } catch (error) {
-      return res.status(400).json({ error: "Não foi possível encontrar os usuários" });
+      return res.status(400).json({ error: "Não foi possível encontrar os produtos" });
+    }
+  }
+
+  static async getProductById(req, res) {
+    const { productId } = req.params
+    try {
+      const pId = await database.Products.findAll({
+        where: {
+          id: Number(productId)
+        }
+      });
+      return res.status(200).json(pId);
+    } catch (error) {
+      return res.status(400).json({ error: "Não foi possível encontrar o produto" });
     }
   }
 
--- server/routes/ProductsRouter.js
+++ server/routes/ProductsRouter.js
@@ -3,7 +3,7 @@ const ProductsController = require("../controller/ProductsController")
 const router = Router()
 
 router.get('/', ProductsController.getAllProducts);
-// router.get('/:productId', ProductsController.getProductById);
+router.get('/:productId', ProductsController.getProductById);
 // router.post('/', ProductsController.createProduct);
 // router.put('/:productId', ProductsController.updateProduct);
 // router.delete('/:productId', ProductsController.deleteProduct);
