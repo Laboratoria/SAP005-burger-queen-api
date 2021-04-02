746b168b2b49d86435d612daf4622f19a08439b3
RobertaKelly <you@example.com>
2021-04-02 20:04:05 -0300

    deploy2

--- index.js
+++ index.js
@@ -1,26 +1,12 @@
-/* eslint-disable linebreak-style */
-// eslint-disable-next-line linebreak-style
-// eslint-disable-next-line linebreak-style
+/* eslint-disable eol-last */
 /* eslint-disable no-console */
-/* eslint-disable linebreak-style */
-// eslint-disable-next-line linebreak-style
-/* eslint-disable quotes */
-/* eslint-disable import/order */
 /* eslint-disable semi */
-/* eslint-disable linebreak-style */
-// eslint-disable-next-line linebreak-style
-require('dotenv').config();
-
-const express = require('express');
-
-const routes = require('./server/routes/index');
-
+/* eslint-disable import/newline-after-import */
+const express = require('express')
+const routes = require('./server/routes/index')
 const app = express()
-const port = process.env.PORT || 5432
+const port = process.env.PORT || 3000;
 
-// eslint-disable-next-line import/newline-after-import
-const cors = require("cors");
-app.use(cors());
 app.use(express.json());
 
 app.use('/', routes);
@@ -31,5 +17,4 @@ app.get('*', (req, res) => {
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
-// eslint-disable-next-line eol-last
 })
